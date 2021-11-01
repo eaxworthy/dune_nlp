94fb319433eecd7715999cd8b9623ce815aa0e9e
Lizzy <axworthy@sonoma.edu>
2021-11-01 11:31:42 -0700

    Switched to Jupyter notebook for analysis. Edited dune text to remove uneeded publishing information. Started txt file for report.

--- script.py
+++
@@ -1,8 +0,0 @@
-import nltk
-from nltk.corpus import PlaintextCorpusReader
-
-nltk.download('gutenberg')
-
-corpus0 = PlaintextCorpusReader("data/prepared_texts/", "dune.txt")
-corpus = nltk.Text(corpus0.words())
-print(corpus.concordance('Idaho'))
